d680b2b8cb2d647f2435db6b3dd656abfd235e5a
Mathias Leimgruber <m.leimgruber@webcloud7.ch>
2026-01-20 12:00:48 -0500

    Remove old upgrade steps.

--- collective/deletepermission/upgrades/to1001.py
+++
@@ -1,7 +0,0 @@
-
-def upgrade(setup_context):
-    setup_context.runImportStepFromProfile(
-        'profile-collective.deletepermission.upgrades:1001',
-        'actions',
-        run_dependencies=False,
-        purge_old=False)
