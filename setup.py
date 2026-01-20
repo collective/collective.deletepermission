d0c40a24c9d813eda4bf4175133be58f09ec7b92
Mathias Leimgruber <m.leimgruber@webcloud7.ch>
2026-01-20 12:07:09 -0500

    Add git actions testing workflow.

--- setup.py
+++ setup.py
@@ -1,89 +1,88 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.5.1.dev0'
+version = "1.5.1.dev0"
 
 tests_require = [
-    'AccessControl',
-    'Products.CMFCore',
-    'Products.GenericSetup',
-    'Products.statusmessages',
-    'Zope',
-    'beautifulsoup4',
-    'requests',
-    'plone.api >= 1.3.0',
-    'plone.app.contenttypes',
-    'plone.app.dexterity',
-    'plone.app.portlets',
-    'plone.app.testing',
-    'plone.autoform',
-    'transaction',
-    'zExceptions',
-    'zope.interface',
-    ]
+    "AccessControl",
+    "Products.CMFCore",
+    "Products.GenericSetup",
+    "Products.statusmessages",
+    "Zope",
+    "beautifulsoup4",
+    "requests",
+    "plone.api >= 1.3.0",
+    "plone.app.contenttypes",
+    "plone.app.dexterity",
+    "plone.app.portlets",
+    "plone.app.testing",
+    "plone.autoform",
+    "transaction",
+    "zExceptions",
+    "zope.interface",
+]
 
 
 extras_require = {
-    'test': tests_require,
-    }
+    "test": tests_require,
+}
 
 
 long_description = (
-    open('README.rst', encoding='utf-8').read()
-    + '\n' +
-    open(os.path.join('docs', 'HISTORY.txt'), encoding='utf-8').read()
-    + '\n')
+    open("README.rst", encoding="utf-8").read()
+    + "\n"
+    + open(os.path.join("docs", "HISTORY.txt"), encoding="utf-8").read()
+    + "\n"
+)
 
-setup(name='collective.deletepermission',
-      version=version,
-      description="Implements a new permission 'Delete portal content'",
-      long_description=long_description,
-
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name="collective.deletepermission",
+    version=version,
+    description="Implements a new permission 'Delete portal content'",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        'Framework :: Plone',
-        'Framework :: Plone :: 6.0',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
-
-      keywords='collective deletepermission 4teamwork ftw plone',
-      author='4teamwork AG',
-      author_email='mailto:info@4teamwork.ch',
-      url='https://github.com/4teamwork/collective.deletepermission',
-      license='GPL2',
-
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-
-      install_requires=[
-        'AccessControl',
-        'Acquisition',
-        'Products.CMFCore',
-        'Products.CMFPlone',
-        'Products.GenericSetup',
-        'Products.PythonScripts',
-        'ZODB',
-        'Zope',
-        'collective.monkeypatcher',
-        'setuptools',
-        'zope.container',
-        'zope.event',
-        'zope.lifecycleevent',
-        ],
-
-      tests_require=tests_require,
-      extras_require=extras_require,
-
-      entry_points="""
+        "Programming Language :: Python :: 3.13",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.1",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.11",
+    keywords="collective deletepermission 4teamwork ftw plone",
+    author="4teamwork AG",
+    author_email="mailto:info@4teamwork.ch",
+    url="https://github.com/4teamwork/collective.deletepermission",
+    license="GPL2",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["collective"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "AccessControl",
+        "Acquisition",
+        "Products.CMFCore",
+        "Products.CMFPlone",
+        "Products.GenericSetup",
+        "Products.PythonScripts",
+        "ZODB",
+        "Zope",
+        "collective.monkeypatcher",
+        "setuptools",
+        "zope.container",
+        "zope.event",
+        "zope.lifecycleevent",
+    ],
+    tests_require=tests_require,
+    extras_require=extras_require,
+    entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      )
+)
